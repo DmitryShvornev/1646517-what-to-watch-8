307b4553c847a53bd45a194fa3c4bfe86b9a4f05
shvor <shvornev.dmitry@yandex.ru>
2021-10-06 22:08:24 +0300

    Маршрутизация

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
 import MainPage from '../main-page/main-page';
+import MoviePage from '../movie-page/movie-page';
+import MyList from '../my-list/my-list';
+import Player from '../player/player';
+import Review from '../review/review';
+import SignIn from '../sign-in/sign-in';
+import NotFound from '../not-found/not-found';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   title: string;
@@ -6,8 +15,39 @@ type AppProps = {
   year: number;
 }
 
-function App({title, genre, year} : AppProps): JSX.Element {
-  return <MainPage  title = {title}  genre = {genre} year = {year}/>;
+function App({ title, genre, year }: AppProps): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainPage title={title} genre={genre} year={year} />
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <MoviePage />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.MyList}
+          render={() => <SignIn />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+          <MyList/>
+        </PrivateRoute>
+        <Route exact path={AppRoute.Player}>
+          <Player />
+        </Route>
+        <Route exact path={AppRoute.AddReview}>
+          <Review />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignIn />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;

b93be58b3ac47689eb5144ee8ae544a61551da83
shvor <shvornev.dmitry@yandex.ru>
2021-10-28 20:35:04 +0300

    Финальный вариант

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -3,34 +3,44 @@ import { useHistory } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { MouseEvent } from 'react';
 import VideoPlayer from '../video-player/video-player';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 type Props = {
   film: Film;
 }
 
+const PLAY_TIMEOUT = 1000;
 
-function FilmCard({  film }: Props): JSX.Element {
+function FilmCard({ film }: Props): JSX.Element {
   const { name, posterImage } = film;
   const history = useHistory();
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [playing, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (playing) {
+      timeout = setTimeout(() => videoRef.current?.play(), PLAY_TIMEOUT);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+    videoRef.current?.load();
+  }, [playing]);
 
   const onClick = (evt: MouseEvent<HTMLAnchorElement>) => {
     evt.preventDefault();
     history.push(AppRoute.Film);
   };
 
-  const onMouseOver = () => {
-    videoRef.current?.play();
-  };
+  const onMouseOver = () => setIsPlaying(true);
 
-  const onMouseOut = () => {
-    videoRef.current?.load();
-  };
+  const onMouseOut = () => setIsPlaying(false);
+
   return (
     <article className="small-film-card catalog__films-card" onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
       <div className="small-film-card__image">
-        <VideoPlayer src={film.previewVideoLink} poster={posterImage} ref={videoRef}/>
+        <VideoPlayer src={film.previewVideoLink} poster={posterImage} ref={videoRef} />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="blank.html" onClick={onClick}>{name}
